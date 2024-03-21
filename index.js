63576772d58670cdc522782af0474f3b87eae57f
ernestmusau <ernest.musau@student.moringaschool.com>
2024-03-21 19:15:35 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+var num1 =62;
+var num2 =1;
+var multiply = num1 * num2;
+var max = Math.max(num1, num2);
+var min = Math.min(num1, num2);
+var random = Math.floor(Math.random() * (max - min + 1)) + min;
+var num3 =9;
+var num4 =5;
+var mod = num3 % num4
+var numbers = new Set([5, 10, 15, 20])
+var numbersArray = Array.from(numbers);
+var max = Math.max(...numbersArray);
+console.log(max)
 
-// Write your code here
+
+
+
